fdbe0abdf811eba86be829b3365b9aaecb7a8696
Brunassis <64547135+Brunassis@users.noreply.github.com>
2020-06-23 19:11:49 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mariadb',
+    host: 'mysql.dbass.com.br',
+    username: 'webbetim202001',
+    password: 'betim202001',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+};
